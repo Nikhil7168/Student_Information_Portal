0c5d88607bf40ca5964943a44bfe979878815ded
Nikhil Rai <65153427+Nikhil7168@users.noreply.github.com>
2022-06-16 18:39:18 +0530

    Client Code

---
+++ client/src/Pages/StudentsearchStudent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { chatHelper } from '../redux/action/studentAction'
+import {useHistory} from 'react-router-dom'
+
+const StudentsearchStudent = () => {
+    const store = useSelector((store) => store)
+    const history = useHistory()
+    return (
+        <div>
+            {store.student.isAuthenticated ? <><HomeHelper /></> : (history.push('/'))}
+        </div>
+    )
+}
+
+export default StudentsearchStudent
